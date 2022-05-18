1fa85d81fd1244ca3880e248c38ec1b310b01e1a
PEI QI <10846004@ntub.edu.tw>
2022-05-18 21:59:12 +0800

    新增-拍照和相簿讀取功能

--- app/src/main/java/tw/edu/ntub/imd/birc/rxandmvvm/view/activity/MainActivity.kt
+++ app/src/main/java/tw/edu/ntub/imd/birc/rxandmvvm/view/activity/MainActivity.kt
@@ -1,10 +1,12 @@
 package tw.edu.ntub.imd.birc.rxandmvvm.view.activity
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.MenuItem
 import android.view.View
-import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import tw.edu.ntub.imd.birc.rxandmvvm.R
@@ -16,11 +18,11 @@ class MainActivity : AppCompatActivity() {
         val homeFragment = HomeFragment()
         val recordFragment = RecordFragment()
         val userFragemnt = UserFragment()
-
         val addFragment = AddFragment()
-
         val otherFragment = OtherFragment()
+
     }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +37,9 @@ class MainActivity : AppCompatActivity() {
             .commit()
         navigationview.selectedItemId = R.id.f1
         navigationview.setOnNavigationItemSelectedListener(listener)
+
+
+
     }
 
     private var listener = object : BottomNavigationView.OnNavigationItemSelectedListener {
@@ -71,11 +76,40 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    //新版button.setOnClickListener的實作方法(首頁測試用按鈕)
+    //新版button.setOnClickListener的實作方法(歷史測試用按鈕)
     fun btn_onClik(view: View) {
-        val button: Button = findViewById(R.id.button1)
-        //button.setOnClickListener {  },好像不用了刪掉也沒抱錯
+        //val button: Button = findViewById(R.id.button1),好像不用
+        //button.setOnClickListener {  },好像不用了刪掉也沒報錯
             startActivity(Intent(this, MainActivity2::class.java))
 
     }
+
+
+    //開啟相機
+    fun camera(view: View) {
+        val i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(i,123)
+    }
+
+    fun album(view: View) {
+        val i = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(i,456)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==123)
+        {
+            var bmp = data?.extras?.get("data") as Bitmap
+            val foodphoto: ImageView = findViewById(R.id.foodphoto)
+            foodphoto.setImageBitmap(bmp)
+        }else if(requestCode==456){
+            val foodphoto: ImageView = findViewById(R.id.foodphoto)
+            foodphoto.setImageURI(data?.data)
+        }
+    }
+
+
 }
