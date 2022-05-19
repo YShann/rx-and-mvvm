6e6a457bc128657328c5a3171cc926d795f8377d
PEI QI <10846004@ntub.edu.tw>
2022-05-19 15:54:13 +0800

    修改-添加介面(將新增飲食紀錄的介面由fragment改成activity)

---
+++ app/src/main/java/tw/edu/ntub/imd/birc/rxandmvvm/view/activity/AddDietRecordActivity.kt
@@ -0,0 +1,47 @@
+package tw.edu.ntub.imd.birc.rxandmvvm.view.activity
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import tw.edu.ntub.imd.birc.rxandmvvm.R
+
+class AddDietRecordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_diet_record)
+
+
+
+    }
+
+    fun camera(view: View) {
+        val i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(i,123)
+    }
+
+    fun album(view: View) {
+        val i = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(i,456)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==123)
+        {
+            var bmp = data?.extras?.get("data") as Bitmap
+            val foodphoto: ImageView = findViewById(R.id.foodphoto)
+            foodphoto.setImageBitmap(bmp)
+        }else if(requestCode==456){
+            val foodphoto: ImageView = findViewById(R.id.foodphoto)
+            foodphoto.setImageURI(data?.data)
+        }
+    }
+
+
+}
--- app/src/main/java/tw/edu/ntub/imd/birc/rxandmvvm/view/activity/MainActivity.kt
+++ app/src/main/java/tw/edu/ntub/imd/birc/rxandmvvm/view/activity/MainActivity.kt
@@ -1,12 +1,9 @@
 package tw.edu.ntub.imd.birc.rxandmvvm.view.activity
 
 import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.MenuItem
 import android.view.View
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import tw.edu.ntub.imd.birc.rxandmvvm.R
@@ -84,31 +81,13 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
-    //開啟相機
-    fun camera(view: View) {
-        val i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(i,123)
+    //切到添加飲食紀錄的畫面
+    fun goBtn1(view: View) {
+        startActivity(Intent(this, AddDietRecordActivity::class.java))
     }
 
-    fun album(view: View) {
-        val i = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(i,456)
+    fun goBtn2(view: View) {
 
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode==123)
-        {
-            var bmp = data?.extras?.get("data") as Bitmap
-            val foodphoto: ImageView = findViewById(R.id.foodphoto)
-            foodphoto.setImageBitmap(bmp)
-        }else if(requestCode==456){
-            val foodphoto: ImageView = findViewById(R.id.foodphoto)
-            foodphoto.setImageURI(data?.data)
-        }
     }
 
 
