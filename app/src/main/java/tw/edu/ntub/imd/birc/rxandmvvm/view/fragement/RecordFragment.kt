efd499f8104ebe038e025ba0f4f3dd0f93e49139
PEI QI <10846004@ntub.edu.tw>
2022-06-02 11:53:39 +0800

    新增-歷史紀錄介面(除了輸出紀錄的部分沒有);日期選擇功能

--- app/src/main/java/tw/edu/ntub/imd/birc/rxandmvvm/view/fragement/RecordFragment.kt
+++ app/src/main/java/tw/edu/ntub/imd/birc/rxandmvvm/view/fragement/RecordFragment.kt
@@ -1,11 +1,18 @@
 package tw.edu.ntub.imd.birc.rxandmvvm.view.fragement
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
 import tw.edu.ntub.imd.birc.rxandmvvm.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,12 +29,19 @@ class RecordFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var dateEdit: EditText
+    //private lateinit var adapter : Adapter
+    private lateinit var spinner : Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        //val adapter = ArrayAdapter.createFromResource(this, R.array.lunch, android.R.layout.simple_spinner_dropdown_item)
+        //spinner.adapter = adapter
+
     }
 
     override fun onCreateView(
@@ -35,7 +49,62 @@ class RecordFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record, container, false)
+        //return inflater.inflate(R.layout.fragment_record, container, false)
+        val root = inflater.inflate(R.layout.fragment_record, container, false)
+        dateEdit = root.findViewById(R.id.date_edit)
+        dateEdit.setOnClickListener(dlistener)
+
+        spinner = root.findViewById(R.id.spinner)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this.requireContext(),
+            R.array.lunch, android.R.layout.simple_spinner_dropdown_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+                //return Toast.makeText(this, "你選的是" + lunch[pos], Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+
+        return root
+    }
+
+
+    private val calender: Calendar = Calendar.getInstance()
+    private val dlistener = View.OnClickListener {
+
+        when (it) {
+
+            dateEdit -> datePicker()
+
+        }
+    }
+
+    private fun datePicker() {
+        DatePickerDialog(
+            this.requireContext(),
+            dateListener,
+            calender.get(Calendar.YEAR),
+            calender.get(Calendar.MONTH),
+            calender.get(Calendar.DAY_OF_MONTH)).show()
+    }
+
+    private val dateListener = DatePickerDialog.OnDateSetListener { _, year, month, day ->
+        calender.set(year, month, day)
+        format("yyyy / MM / dd", dateEdit)
+    }
+
+    private fun format(format: String, view: View) {
+        val time = SimpleDateFormat(format, Locale.TAIWAN)
+        (view as EditText).setText(time.format(calender.time))
     }
 
     companion object {
