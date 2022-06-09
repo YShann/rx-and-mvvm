da44e0867891d395c264bc8a48cf40903f1a8593
Luhanhan33 <ivy9842@gmail.com>
2022-06-09 11:20:25 +0800

    新增-註冊畫面

--- app/src/main/java/tw/edu/ntub/imd/birc/rxandmvvm/RegisterActivity.kt
+++ app/src/main/java/tw/edu/ntub/imd/birc/rxandmvvm/RegisterActivity.kt
@@ -1,11 +1,4 @@
 package tw.edu.ntub.imd.birc.rxandmvvm
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
-class RegisterActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
-    }
+class RegisterActivity {
 }
