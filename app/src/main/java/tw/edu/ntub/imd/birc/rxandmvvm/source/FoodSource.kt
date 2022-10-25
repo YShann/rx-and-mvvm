9af1d19d2221716f1ea93ac8b7f98c238a77aa24
PEI QI <10846004@ntub.edu.tw>
2022-10-25 22:44:47 +0800

    刪除-food各個檔

--- app/src/main/java/tw/edu/ntub/imd/birc/rxandmvvm/source/FoodSource.kt
+++
@@ -1,9 +0,0 @@
-package tw.edu.ntub.imd.birc.rxandmvvm.source
-
-import io.reactivex.rxjava3.core.Observable
-import tw.edu.ntub.imd.birc.rxandmvvm.data.Food
-import tw.edu.ntub.imd.birc.rxandmvvm.data.ResponseBody
-
-interface FoodSource {
-    fun searchAll(): Observable<SourceState<ResponseBody<Food>>>
-}
