7356ffba800f3424372cc4a17de5818591f9102c
YShann <luyingshan16@gmail.com>
2022-11-11 17:18:17 +0800

    修復pieChart問題

--- app/src/main/java/tw/edu/ntub/imd/birc/rxandmvvm/view/activity/HomeActivity.kt
+++ app/src/main/java/tw/edu/ntub/imd/birc/rxandmvvm/view/activity/HomeActivity.kt
@@ -1,34 +1,30 @@
 package tw.edu.ntub.imd.birc.rxandmvvm.view.activity
 
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.PopupMenu
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import tw.edu.ntub.imd.birc.rxandmvvm.R
 import tw.edu.ntub.imd.birc.rxandmvvm.extension.attachToRecyclerView
 import tw.edu.ntub.imd.birc.rxandmvvm.extension.mapSourceState
 import tw.edu.ntub.imd.birc.rxandmvvm.view.adapter.ObservableAdapter
-import tw.edu.ntub.imd.birc.rxandmvvm.view.adapter.item.DietRecordItem
 import tw.edu.ntub.imd.birc.rxandmvvm.view.adapter.item.HomeDietRecordItem
 import tw.edu.ntub.imd.birc.rxandmvvm.view.fragement.*
 import tw.edu.ntub.imd.birc.rxandmvvm.viewmodel.DietRecordViewModel
 import java.text.SimpleDateFormat
-import java.time.Year
 import java.util.*
+import kotlin.math.floor
 
 
 class HomeActivity : AppCompatActivity() {
@@ -48,6 +44,7 @@ class HomeActivity : AppCompatActivity() {
 
     companion object {
         val homeFragment = HomeFragment()
+
         //原本是val recordFragment = RecordFragment()被警告有內存洩漏的問題，按照建議改成下面那行
         val recordFragment by lazy { RecordFragment() }
         val createRecordFragment by lazy { CreateDietRecordFragment() }
@@ -81,19 +78,54 @@ class HomeActivity : AppCompatActivity() {
         homeToolbarTitle.text =
             dateInString.replace("/", "月").plus("日（${date.getToday_Chinaname()}）")
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-        showPieChart(pieChart)
         val year = Calendar.getInstance().get(Calendar.YEAR)
         val month = Calendar.getInstance().get(Calendar.MONTH).plus(1)
         val day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
         val startDate = "${year}-${month}-${day}"
         val endDate = "${year}-${month}-${day.plus(1)}"
-        Log.d("startDate",startDate)
-        Log.d("endDate",endDate)
+        var grainCount = 0
+        var vegetableCount = 0
+        var meatCount = 0
+        var milkCount = 0
+        var fruitCount = 0
+        var fatCount = 0
         val adapter = ObservableAdapter(
             viewModel.searchByMealTime(startDate, endDate)
-                .mapSourceState { it.data.map { dietRecord -> HomeDietRecordItem(dietRecord) } }
+                .mapSourceState {
+                    it.data.map { dietRecord ->
+                        if (dietRecord.grains == "1") {
+                            grainCount += 1
+                        }
+                        if (dietRecord.vegetables == "1") {
+                            vegetableCount += 1
+                        }
+                        if (dietRecord.meatsAndProtein == "1") {
+                            meatCount += 1
+                        }
+                        if (dietRecord.milkAndDairy == "1") {
+                            milkCount += 1
+                        }
+                        if (dietRecord.fruits == "1") {
+                            fruitCount += 1
+                        }
+                        if (dietRecord.fats == "1") {
+                            fatCount += 1
+                        }
+                    }
+                    showPieChart(
+                        pieChart,
+                        grainCount,
+                        vegetableCount,
+                        meatCount,
+                        milkCount,
+                        fruitCount,
+                        fatCount
+                    )
+                    it.data.map { dietRecord -> HomeDietRecordItem(dietRecord) }
+                }
         )
         adapter.attachToRecyclerView(homeFoodRecycler)
+
 
         homeFoodToDateBtn.setOnClickListener {
             supportFragmentManager.beginTransaction()
@@ -138,47 +170,6 @@ class HomeActivity : AppCompatActivity() {
     }
 
 
-//                    //        //設定初始時要show哪個fragment
-//            navigationview = findViewById<BottomNavigationView>(R.id.main_navigationView)
-//            supportFragmentManager.beginTransaction()
-//                .replace(R.id.container_activity_main, recordFragment)
-//                .commit()
-//            navigationview.selectedItemId = R.id.f2
-//            navigationview.setOnNavigationItemSelectedListener(listener)
-
-
-    //    private var listener = object : BottomNavigationView.OnNavigationItemSelectedListener {
-//        //設定navigationBar裡的item們的點擊事件 被點擊後採動態載入fragment的方式
-//        override fun onNavigationItemSelected(item: MenuItem): Boolean {
-//            when (item.itemId) {
-////                R.id.f1 -> {
-////                    val manager = supportFragmentManager
-////                    val transaction = manager.beginTransaction()
-////                    transaction.replace(R.id.container_activity_main, homeFragment).commit()
-////
-////                }
-//                R.id.f2 -> {
-//                    val t = supportFragmentManager.beginTransaction()
-//                    t.replace(R.id.container_activity_main, recordFragment).commit()
-//                }
-//                R.id.f3 -> {
-//                    val t = supportFragmentManager.beginTransaction()
-//                    t.replace(R.id.container_activity_main, addFragment).commit()
-//                }
-//
-//                R.id.f4 -> {
-//                    val t = supportFragmentManager.beginTransaction()
-//                    t.replace(R.id.container_activity_main, userFragemnt).commit()
-//                }
-//
-////                R.id.f5 -> {
-////                    val t = supportFragmentManager.beginTransaction()
-////                    t.replace(R.id.container_activity_main, otherFragment).commit()
-////                }
-//            }
-//            return true
-//        }
-
     ///////////////////////////////////////////////////////////////////////////////
     fun Date.toString(format: String, locale: Locale = Locale.getDefault()): String {
         val formatter = SimpleDateFormat(format, locale)
@@ -202,20 +193,26 @@ class HomeActivity : AppCompatActivity() {
     }
     ///////////////////////////////////////////////////////////////////////////////
 
-    private fun showPieChart(pieChart: PieChart) {
-        val pieEntries: ArrayList<PieEntry> = ArrayList()
+    private fun showPieChart(
+        pieChart: PieChart,
+        grains: Int,
+        vegetables: Int,
+        meats: Int,
+        milk: Int,
+        fruits: Int,
+        fats: Int
+    ) {
+//        val pieEntries: ArrayList<PieEntry> = ArrayList()
         val label = ""
-
+        val allCount = grains + vegetables + meats + milk + fruits + fats
         //initializing data
-
-        //initializing data
-        val typeAmountMap: MutableMap<String, Int> = HashMap()
-        typeAmountMap["全榖雜糧"] = 1
-        typeAmountMap["蛋豆魚肉"] = 1
-        typeAmountMap["乳品"] = 1
-        typeAmountMap["蔬菜"] = 1
-        typeAmountMap["水果"] = 2
-        typeAmountMap["油脂與堅果種子"] = 1
+//        val typeAmountMap: MutableMap<String, Int> = HashMap()
+//        typeAmountMap["全榖雜糧"] = grains / allCount
+//        typeAmountMap["蛋豆魚肉"] = meats / allCount
+//        typeAmountMap["乳品"] = milk / allCount
+//        typeAmountMap["蔬菜"] = vegetables / allCount
+//        typeAmountMap["水果"] = fruits / allCount
+//        typeAmountMap["油脂與堅果種子"] = fats / allCount
 
         val colors: ArrayList<Int> = ArrayList()
         colors.add(resources.getColor(R.color.pieChart_0))
@@ -226,21 +223,36 @@ class HomeActivity : AppCompatActivity() {
         colors.add(resources.getColor(R.color.pieChart_5))
 
 
-        for (type in typeAmountMap.keys) {
-            pieEntries.add(PieEntry(typeAmountMap[type]!!.toFloat(), type))
-        }
+//        for (type in typeAmountMap.keys) {
+//            pieEntries.add(PieEntry(typeAmountMap[type]!!.toFloat(), type))
+//        }
+        val pieEntries = ArrayList<PieEntry>()
+        pieEntries.add(PieEntry((grains / allCount).toFloat(),"全榖雜糧"))
+        pieEntries.add(PieEntry((meats / allCount).toFloat(),"蛋豆魚肉"))
+        pieEntries.add(PieEntry((milk / allCount).toFloat(),"乳品"))
+        pieEntries.add(PieEntry((vegetables / allCount).toFloat(),"蔬菜"))
+        pieEntries.add(PieEntry((fruits / allCount).toFloat(),"水果"))
+        pieEntries.add(PieEntry((fats / allCount).toFloat(),"油脂與堅果種子"))
+
 
         val pieDataSet = PieDataSet(pieEntries, label)
         pieDataSet.valueTextSize = 14f
         pieDataSet.colors = colors
         pieChart.description.isEnabled = false;
+        pieChart.setUsePercentValues(true)
+        pieDataSet.valueFormatter = object : ValueFormatter() {
+            override fun getFormattedValue(value: Float): String {
+                if (value == 0.0f)
+                    return "";
+                return floor(value).toString().replace(".0","%")
+            }
+        }
         pieChart.legend.textSize = 14f
         pieChart.legend.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
         pieChart.legend.verticalAlignment = Legend.LegendVerticalAlignment.CENTER
         pieChart.legend.orientation = Legend.LegendOrientation.VERTICAL
         pieChart.legend.setDrawInside(false)
         pieChart.setDrawSliceText(false)
-//        pieChart.animateY(1400, Easing.EaseInOutQuad);
 
         val pieData = PieData(pieDataSet)
         pieData.setDrawValues(true)
